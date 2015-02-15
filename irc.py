51cdbf34f0c0293b1c6840c9da1f3f9107a2ba44
Jack Cook <jack@jackcook.nyc>
2015-02-15 01:31:05 -0500

    Updated variables and stuff

--- irc.py
+++ irc.py
@@ -1,26 +1,27 @@
 import socket
 import string
 
-readbuffer = ""
+def run():
+    readbuffer = ""
 
-s = socket.socket()
-s.connect(("irc.esper.net", 6667))
-s.send("NICK hackbot_____\r\n")
-s.send("USER hackbot irc.esper.net bla :HackBot\r\n")
+    s = socket.socket()
+    s.connect(("irc.esper.net", 6667))
+    s.send("NICK hackbot_\r\n")
+    s.send("USER hackbot irc.esper.net bla :HackBot\r\n")
 
-while True:
-    readbuffer = readbuffer + s.recv(1024)
-    temp = string.split(readbuffer, "\n")
-    readbuffer = temp.pop()
+    while True:
+        readbuffer = readbuffer + s.recv(1024)
+        temp = string.split(readbuffer, "\n")
+        readbuffer = temp.pop()
 
-    for line in temp:
-        line = string.rstrip(line).split(line)
-        l = line[0].split(" ")
-        print line
-        if l[0] == "PING":
-            print "ping"
-            s.send("PONG %s\r\n" % l[1])
-        if len(l) > 4:
-            if l[3] == ":End":
-                print "joining..."
-                s.send("JOIN hawkfalcon")
+        for line in temp:
+            line = string.rstrip(line).split(line)
+            l = line[0].split(" ")
+            print line[0]
+            if l[0] == "PING":
+                s.send("PONG %s\r\n" % l[1])
+            if len(l) > 4:
+                if l[3] == ":End":
+                    s.send("JOIN #hackcooper\r\n")
+
+run()
