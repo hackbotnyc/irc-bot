ba28e53cd79574fccde68072086f01eed6062fc3
Jack Cook <jack@jackcook.nyc>
2015-02-15 01:51:08 -0500

    Added basic commands system

--- irc.py
+++ irc.py
@@ -6,7 +6,7 @@ def run():
 
     s = socket.socket()
     s.connect(("irc.esper.net", 6667))
-    s.send("NICK hackbot_\r\n")
+    s.send("NICK hackbot\r\n")
     s.send("USER hackbot irc.esper.net bla :HackBot\r\n")
 
     while True:
@@ -16,12 +16,21 @@ def run():
 
         for line in temp:
             line = string.rstrip(line).split(line)
-            l = line[0].split(" ")
             print line[0]
+            l = line[0].split(" ")
             if l[0] == "PING":
+                print "pong"
                 s.send("PONG %s\r\n" % l[1])
-            if len(l) > 4:
-                if l[3] == ":End":
+            if len(l) >= 4:
+                l.remove(l[0])
+                l.remove(l[0])
+                l.remove(l[0])
+                l[0] = l[0].replace(":", "")
+                args = l
+                
+                if args[0] == "End":
                     s.send("JOIN #hackcooper\r\n")
+                elif args[0] == "!move":
+                    print "OMG MOVE FORWARD"
 
 run()
